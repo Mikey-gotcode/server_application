209bb26a9aa67f6621c95db8a318db6c983b049a
dedanl <123885283+dedanl@users.noreply.github.com>
2024-07-16 05:46:08 +0300

    URI changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports={
-    mongoURI:'mongodb+srv://spidungu_barbosa:Mw@ng!X18@ma3cluster.x9epetw.mongodb.net/?retryWrites=true&w=majority&appName=MA3CLUSTER',
-    secretOrKey:'secret'
-}
+module.exports = {
+    mongoURI: 'mongodb+srv://spidungu_barbosa:Mw@ng!X18@ma3cluster.x9epetw.mongodb.net/MA3database?retryWrites=true&w=majority&appName=MA3CLUSTER',
+    secretOrKey: 'secret'
+  };
+  
